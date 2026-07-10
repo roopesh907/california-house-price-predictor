4fabb313d6fcd66ec56ea73147325075d1559080
roopesh <roopeshmallem@gmail.com>
2026-07-10 16:55:10 +0530

    Add Vercel frontend to CORS

--- Backend/main.py
+++ Backend/main.py
@@ -13,6 +13,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:5174",
+        "https://california-house-price-predictor.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
